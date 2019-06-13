a5059d824c6c9d2ea425afe3f02e943bbbb657a5
Markus Felten <Markus.Felten@gmx.de>
2019-06-13 22:15:14 +0200

    fix: use fetch

--- index.mjs
+++ index.mjs
@@ -1,5 +1,10 @@
+import fetch from "node-fetch";
 
+export async function npmDepAnalyser(name,version, categorizer) {
 
-export function npmDepAnalyser(name,version, categorizer) {
-
+    const result = await fetch(`https://registry.npmjs.org/${name}`);
+    const pkg = await result.json();
+    console.log(pkg);
 }
+
+
