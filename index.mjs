1d252aac01340ca54961a74dd4672aa879b315cb
Markus Felten <Markus.Felten@gmx.de>
2022-04-29 20:47:09 +0200

    wip

--- index.mjs
+++ index.mjs
@@ -1,15 +1,11 @@
-//import { fetch } from 'fetch-h2';
-import pacote  from 'pacote';
+import pacote from "pacote";
 
-export async function npmDepAnalyser(name,version, categorizer) {
+export async function npmDepAnalyser(name, version, categorizer) {
+  const pkg = await pacote.manifest("pacote@^1");
 
-    const pkg = await pacote.manifest('pacote@^1');
-
-    /*
+  /*
     const result = await fetch(`https://registry.npmjs.org/${name}`);
     const pkg = await result.json();
     */
-    console.log(pkg);
+  console.log(pkg);
 }
-
-
